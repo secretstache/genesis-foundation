30cd8fdfb4997d12d290a394d6ca1ce08fad8d40
Rich Staats <rich@secretstache.com>
2016-07-27 11:58:39 -0600

    fixes f6 menus

--- lib/foundation/foundation-functions.php
+++ lib/foundation/foundation-functions.php
@@ -5,7 +5,9 @@ add_action('genesis_before', 'ssm_open_offnav_markup', 10);
  * Add Foundation offcanvas opening markup
  */
 function ssm_open_offnav_markup() {
-	echo '<div class="off-canvas-wrapper"><div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>';
+  $offnavopen = '<div class="off-canvas-wrapper">';
+  $offnavopen .= '<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>';
+	echo $offnavopen;
 }
 
 add_action('genesis_after', 'ssm_close_offnav_markup', 5);
@@ -13,7 +15,9 @@ add_action('genesis_after', 'ssm_close_offnav_markup', 5);
  * Add Foundation offcanvas closing markup
  */
 function ssm_close_offnav_markup() {
-	echo '</div><!-- end .off-canvas-wrap --></div></div>';
+  $offnavclose = '</div><!-- end .off-canvas-wrapper-inner -->';
+  $offnavclose .= '</div><!-- end off-canvas-wrapper -->';
+	echo $offnavclose;
 }
 
 /**
@@ -30,7 +34,7 @@ ADD FOUNDATION FEATURES TO WORDPRESS
 
 //Use the active class of the ZURB Foundation for the current menu item. (From: https://github.com/milohuang/reverie/blob/master/functions.php)
 function required_active_nav_class( $classes, $item ) {
-    if ( $item->current == 1 || $item->current_item_ancestor == true ) {
+    if ( $item->current == 1 ) {
         $classes[] = 'active';
     }
     return $classes;
@@ -49,14 +53,16 @@ register_nav_menus(
 function ssm_primary_navigation() {
 	// display the wp3 menu if available
     wp_nav_menu(array(
-		'container'	=> false,
-    	'menu' => 'Primary Navigation', 
-    	'menu_class'	=> 'genesis-nav-menu', 				// nav name
-    	'theme_location' => 'primary-navigation',       // where it's located in the theme
-    	'before' => '',                                 // before the menu
-        'after' => '',                                  // after the menu
-        'link_before' => '',                            // before each link
-        'link_after' => '',                             // after each link
+      'container'	=> false,
+      'menu' => 'Primary Navigation', 
+      'menu_class'	=> 'dropdown menu', 				// nav name
+      'theme_location' => 'primary-navigation',       // where it's located in the theme
+      'before' => '',                                 // before the menu
+      'after' => '',                                  // after the menu
+      'link_before' => '',                            // before each link
+      'link_after' => '', 
+      'items_wrap' => '<ul id="%1$s" class="%2$s" data-dropdown-menu>%3$s</ul>',
+      'walker'  => new Foundation_Walker()                            // after each link
 	));
 
 }
@@ -64,15 +70,15 @@ function ssm_primary_navigation() {
 function ssm_primary_mobile_navigation() {
 	// display the wp3 menu if available
     wp_nav_menu(array(
-		'container'	=> false,
-    	'menu' => 'Primary Navigation',  				// nav name
-		'menu_class' =>	'off-canvas-list',				// ul class			
-    	'theme_location' => 'primary-navigation',       // where it's located in the theme
-    	'before' => '',                                 // before the menu
-        'after' => '',                                  // after the menu
-        'link_before' => '',                            // before each link
-        'link_after' => '',                             // after each link
-		'walker'	=> new Foundation_Walker()
+      'container'	=> false,
+      'menu' => 'Primary Navigation',  				// nav name
+      'menu_class' =>	'vertical menu',				// ul class			
+      'theme_location' => 'primary-navigation',       // where it's located in the theme
+      'before' => '',                                 // before the menu
+      'after' => '',                                  // after the menu
+      'link_before' => '',                            // before each link
+      'link_after' => '',                             // after each link
+      'walker'	=> new Foundation_Walker()
 	));
 
 }
@@ -93,17 +99,13 @@ function ssm_do_primary_navigation() { ?>
 
 		<div class="hide-for-large mobile-menu">
 			<div class="title-bar">
-				 <button class="menu-icon" type="button" data-open="offCanvas"></button>
+				 <button class="menu-icon" type="button" data-toggle="offCanvas"></button>
 			</div>
 		</div>
 
 		<div class="off-canvas position-left" id="offCanvas" data-off-canvas data-position="left">
-			<div class="off-canvas-content" data-off-canvas-content>
-				<ul class="vertical menu">
-					<li><label>Navigation</label></li>
-				</ul>
-				
-				<?php ssm_primary_navigation(); ?>
+			<div class="off-canvas-content" data-off-canvas-content>				
+				<?php ssm_primary_mobile_navigation(); ?>
 			</div>
 		</div>
 		
