3359d9227f8c79e3da3a63315748660ea25717bf
Rich Staats <rich@secretstache.com>
2016-08-11 20:06:31 -0600

    forces full width on landing page template

--- page-landing.php
+++ page-landing.php
@@ -2,6 +2,9 @@
 
 //* Template Name: Landing Page
 
+// Force full width layout */
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
 
 add_filter( 'body_class', 'ssm_landing_page_class' );
 /*
